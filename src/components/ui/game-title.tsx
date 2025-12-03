33c1bf4986cf1a1b5000a44313d9cc371daf6dd4
Exhigh <exhigh01@gmail.com>
2025-12-03 17:45:08 +0400

    Address Review Comment
    * Refactor the game title scroll to a component.

---
+++ src/components/ui/game-title.tsx
@@ -0,0 +1,88 @@
+import { RefObject, useEffect, useRef, useState } from "react";
+
+interface GameTitleProps {
+    ref: RefObject<HTMLDivElement | null>;
+    title: string;
+    classNames: string;
+}
+
+export function GameTitle(props: GameTitleProps) {
+    const [transformLimit, setTransformLimit] = useState(0);
+    const [titleBoxWidth, setTitleBoxWidth] = useState(150);
+    const [titleAnimate, setTitleAnimate] = useState<Boolean>(false);
+    const [windowWidth, setWindowWidth] = useState(0.0);
+    const gameTitleRef = useRef<HTMLSpanElement>(null);
+
+    const updateDimensions = () => {
+        setWindowWidth(props.ref.current!.clientWidth)
+    };
+
+    const handleTransformLimit = (num: number) => {
+        if (num != transformLimit) {
+            setTransformLimit(num);
+        }
+    };
+
+    const handleTitleBoxWidth = (num: number) => {
+        if (num != titleBoxWidth) {
+            setTitleBoxWidth(num);
+        }
+    };
+
+    const handleTitleAnimate = (state: boolean) => {
+        if (state != titleAnimate) {
+            setTitleAnimate(state);
+        }
+    };
+
+    const marqueeTransformStyle = {
+        '--transform-final': `${transformLimit}px`,
+    } as React.CSSProperties;
+
+    useEffect(() => {
+        if (props.ref.current && gameTitleRef.current) {
+            window.addEventListener("resize", updateDimensions);
+        }
+        
+        return () => {
+            window.removeEventListener("resize", updateDimensions);
+        };
+    }, [gameTitleRef]);
+
+    useEffect(() => {
+        handleTitleAnimate(textWrapAnimate());
+    }, [windowWidth]);
+
+    function textWrapAnimate() {
+        if (gameTitleRef.current) {
+            if (props.title.length >= 17) {
+                const boxLength = Math.ceil((gameTitleRef.current.getBoundingClientRect()).width);
+                if (boxLength > 170 && props.title.length < 19) {
+                    return false;
+                }
+                handleTitleBoxWidth(boxLength);
+                const upper = (props.title.match(/[A-Z]/g) || []).length;
+                const special = (props.title.match(/[\s\W]/g) || []).length;
+                const lower = props.title.length - (upper + special);
+                const fit = Math.ceil(
+                    (((titleBoxWidth * 0.15 + (upper * 18) + (lower * 17) + (special * 20) + titleBoxWidth) +
+                        ((props.title.length - 20) * 50)) / (titleBoxWidth * 0.85)) * 18);
+
+                handleTransformLimit(-fit);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    };
+
+    return (
+        <span ref={gameTitleRef} className={`${titleAnimate ? ('animate-marquee' + " ") : ''}` + `${props.classNames}`}
+            style={marqueeTransformStyle}
+        >
+            {props.title}
+        </span>
+    );
+}
